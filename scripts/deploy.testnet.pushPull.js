b5880ee8ea9fa7afb44c09ee0cda2e448ee361ce
Farhad <farhad.szd@gmail.com>
2024-04-17 12:00:03 +0400

    updated deploy scripts

---
+++ scripts/deploy.testnet.pushPull.js
@@ -0,0 +1,24 @@
+const { ethers, upgrades } = require("hardhat");
+require("dotenv").config();
+
+const TEST_TOKEN = process.env.TEST_TOKEN;
+async function main() {
+  const PushPull = await ethers.getContractFactory("PushPull");
+
+  console.log("Deploying PushPull...");
+  const pushPull = await upgrades.deployProxy(PushPull, [TEST_TOKEN], {
+    initializer: "initialize",
+    kind: "transparent",
+  });
+  console.log(`Waiting for 3 confirmations...`);
+  await pushPull.deploymentTransaction().wait(3);
+
+  console.log("\x1b[32mPushPull deployed to:", pushPull.target, "\x1b[0m");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
